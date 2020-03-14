ac43d266371a24c41f37f0d626a4883d9d752c95
73cirdan <32799399+73cirdan@users.noreply.github.com>
2020-03-14 16:49:28 +0100

    cleanup and translation

--- node_helper.js
+++ node_helper.js
@@ -32,16 +32,19 @@ module.exports = NodeHelper.create({
 			url: rainfcUrl,
 			method: 'GET',
 		}, function (error, response, body) {
-			console.log(self.name + ": rain forecast data: " + body);
-        // This is test data just to see the graph if there is no rain
-        body="077|10:05\n034|10:10\n101|10:15\n087|10:20\n077|10:25\n240|10:30\n000|10:35\n000|10:40\n077|10:45\n087|10:50\n087|10:55\n077|11:00\n077|11:05\n034|11:10\n017|11:15\n000|11:20\n000|11:25\n000|11:30\n000|11:35\n000|11:40\n000|11:45\n000|11:50\n100|11:55\n200|12:00\n";
-
-			console.log(self.name + ": rain forecast data: " + body);
+        		// This is test data just to see the graph if there is no rain
+        		//body= "077|10:05\n034|10:10\n101|10:15\n087|10:20\n"+
+				"077|10:25\n240|10:30\n000|10:35\n000|10:40\n"+
+				"077|10:45\n087|10:50\n087|10:55\n077|11:00\n"+
+				"077|11:05\n034|11:10\n017|11:15\n000|11:20\n"+
+				"000|11:25\n000|11:30\n000|11:35\n000|11:40\n"+
+				"000|11:45\n000|11:50\n100|11:55\n200|12:00\n";
+			//console.log(self.name + ": rain forecast data: " + body);
 			
 			if (!error && response.statusCode == 200) {
 				self.sendSocketNotification("DATA", body);
 			} else {
-				error = "No forecast data, will retry";
+				error = "No forecast connection, will retry";
 				console.log(self.name + error);
 				self.sendSocketNotification("ERROR", error);
 			}
