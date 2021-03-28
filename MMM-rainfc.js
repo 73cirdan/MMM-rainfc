4d4f663e0dff01c41002f9a82bbef4273354ee31
73cirdan <73cirdan@gmail.com>
2021-03-28 12:51:46 +0200

    test ok
    Signed-off-by: 73cirdan <73cirdan@gmail.com>

--- MMM-rainfc.js
+++ MMM-rainfc.js
@@ -172,7 +172,7 @@ Module.register("MMM-rainfc",{
 			//Log.info("maxrain = " + this.maxrain);	
 		}
 
-		// issue12
+		// Issue12
 		// sometimes the api returns not for 2 hours of data, but a susbset. We complete the set to 2 hours
 		// there should be 24 entries, times is used to indicate the incompleteness of the answer, by using a #
 		for (i = this.rains.length; i < 24; i++) {
