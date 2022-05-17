4cab53a39ad5d3a8eeb3e76f0c8a6d0f37e3c82c
73cirdan <73cirdan@gmail.com>
2022-05-17 15:14:46 +0200

    replaced request module with axios
    Signed-off-by: 73cirdan <73cirdan@gmail.com>

--- node_helper.js
+++ node_helper.js
@@ -8,8 +8,7 @@
  */
 
 const NodeHelper = require('node_helper');
-var request = require('request');
-var moment = require('moment');
+const axios = require('axios').default;
 
 module.exports = NodeHelper.create({
 
@@ -25,38 +24,33 @@ module.exports = NodeHelper.create({
 	getData: function() {
 		var self = this;
 		
-		var rainfcUrl = this.config.apiBase + "/" + this.config.rainfcEndpoint + "?lat=" + this.config.lat + "&lon="+ this.config.lon;
+		var rainfcUrl =  this.config.apiBase + "/" + this.config.rainfcEndpoint + "?lat=" + this.config.lat + "&lon="+ this.config.lon;
 		console.log(self.name + ": loading rain forecast for : " + rainfcUrl);
 				
-		request({
-			url: rainfcUrl,
-			method: 'GET',
-		}, function (error, response, body) {
-        		// This is test data just to see the graph if there is no rain
-        		//body=   "077|10:05\n034|10:10\n101|10:15\n087|10:20\n"+
-				"077|10:25\n020|10:30\n000|10:35\n000|10:40\n"+
-				"077|10:45\n087|10:50\n087|10:55\n127|11:00\n"+
-				"137|11:05\n034|11:10\n170|11:15\n000|11:20\n"+
-				"000|11:25\n000|11:30\n000|11:35\n000|11:40\n"+
-				"010|11:45\n020|11:50\n030|11:55\n043|12:00\n";
-        		// This is test data just to see how the code handles a incomplete reciept
-        		//body=   "077|10:05\n034|10:10\n101|10:15\n087|10:20\n"+
-				"077|10:25\n020|10:30\n000|10:35\n000|10:40\n"+
-				"077|10:45\n087|10:50\n087|10:55\n127|11:00\n";
-			//console.log(self.name + ": rain forecast data: " + body);
-			
-			if (!error && response.statusCode == 200) {
-				if (!body || body=="") console.log(self.name + body + response.headers);
-				//self.mayhem--;
-				//if (self.mayhem<0 && self.mayhem>-5) { body=null };
-				self.sendSocketNotification("DATA", body);
-			} else {
-				error = "No forecast connection, will retry";
-				console.log(self.name + error);
-				self.sendSocketNotification("ERROR", error);
-			}
-		});
-
+		
+		axios.get(rainfcUrl)
+  			.then(function (response) {
+    				// handle success console.log(response);
+				self.sendSocketNotification("DATA", response.data);
+  			})
+  			.catch(function (error) {
+    				// handle error
+    				if (error.response) {
+      					// The request was made and the server responded with a status code
+      					// that falls out of the range of 2xx
+      					console.log("Error: " + self.name + ": " + error.response.status);
+      					console.log("Error: " + self.name + ": " + error.response.data);
+      					console.log("Error: " + self.name + ": " + error.response.headers);
+    				} else if (error.request) {
+      					// The request was made but no response was received
+      					console.log("Error: " + self.name + ": " + error.request);
+    				} else {
+      					// Something happened in setting up the request that triggered an Error
+      					console.log("Error: " + self.name + ": " + error.message);
+    				}
+    				//console.log(error.config);
+				self.sendSocketNotification("ERROR", self.name + "No forecast connection, will retry");
+  			})
 		setTimeout(function() { self.getData(); }, this.config.refreshInterval);
 	},
 
@@ -68,7 +62,22 @@ module.exports = NodeHelper.create({
 			self.getData();
 			self.started = true;
 			console.log(self.name + ": configured");
-			//self.mayhem=2;
 		}
 	}
 });
+
+
+/*
+        		// This is test data just to see the graph if there is no rain
+        		body=   "077|10:05\n034|10:10\n101|10:15\n087|10:20\n"+
+				"077|10:25\n020|10:30\n000|10:35\n000|10:40\n"+
+				"077|10:45\n087|10:50\n087|10:55\n127|11:00\n"+
+				"137|11:05\n034|11:10\n170|11:15\n000|11:20\n"+
+				"000|11:25\n000|11:30\n000|11:35\n000|11:40\n"+
+				"010|11:45\n020|11:50\n030|11:55\n043|12:00\n";
+        		// This is test data just to see how the code handles a incomplete receipt
+        		body=   "077|10:05\n034|10:10\n101|10:15\n087|10:20\n"+
+				"077|10:25\n020|10:30\n000|10:35\n000|10:40\n"+
+				"077|10:45\n087|10:50\n087|10:55\n127|11:00\n";
+			console.log(self.name + ": rain forecast data: " + body);
+*/
